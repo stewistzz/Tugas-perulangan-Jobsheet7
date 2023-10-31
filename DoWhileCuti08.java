5a62e3ba4d493d11241c121c71f287707a44d9bc
unknown <dimassetyonugroho30@gmail.com>
2023-10-31 20:57:21 +0700

    mengulang input saat jumlah hari lebih dari jatah libur

--- DoWhileCuti08.java
+++ DoWhileCuti08.java
@@ -11,6 +11,25 @@ public class DoWhileCuti08 {
         jatahCuti = sc.nextInt();
 
         // perulangan do-while
+        // do {
+        //     System.out.print("Apakah anda ingin mengambil cuti? (y/t)");
+        //     konfirmasi = sc.next();
+
+        //     if (konfirmasi.equalsIgnoreCase("y")) {
+        //         System.out.print("Jumlah hari : ");
+        //         jumlahHari = sc.nextInt();
+        //             if (jumlahHari <= jatahCuti) {
+        //                  jatahCuti -= jumlahHari;
+        //                  System.out.println("sisa jatah cuti " + jatahCuti+ " hari");
+        //             } else {
+        //                 System.out.println("sisa jatah cuti anda tidak mencukupi");
+        //                 break;
+        //          }
+
+        //     }
+        // } while(jatahCuti > 0);
+
+
         do {
             System.out.print("Apakah anda ingin mengambil cuti? (y/t)");
             konfirmasi = sc.next();
@@ -23,12 +42,15 @@ public class DoWhileCuti08 {
                     jatahCuti -= jumlahHari;
                     System.out.println("sisa jatah cuti " + jatahCuti + " hari");
                 } else {
-                    System.out.println("sisa jatah cuti anda tidak mencukupi");
-                    break;
+                    System.out.println("Jumlah hari cuti anda melebihi batas cuti yang diberikan");
+                    // System.out.print("Masukkan jumlah hari lagi : ");
+                    // // jatahCuti = sc.nextInt();
+                    // jumlahHari = sc.nextInt();
                 }
 
             }
         } while(jatahCuti > 0);
+        // } while(jatahCuti > 0 && konfirmasi.equalsIgnoreCase("y"));
 
     }
 }
